4e70712a8e67feb50d5eb131329af7f0efc78aa9
zixi01.chen <zixi01.chen@horizon.ai>
2026-05-15 14:55:10 +0800

    fix(launch): fix launch model usage.

--- launch/hobot_vis_render.launch.py
+++ launch/hobot_vis_render.launch.py
@@ -30,10 +30,7 @@ def generate_launch_description():
                 get_package_share_directory('dnn_node_example'),
                 'launch/dnn_node_example.launch.py')),
         launch_arguments={
-            'dnn_example_config_file': 'config/fcosworkconfig.json',
-            'dnn_example_msg_pub_topic_name': '/hobot_dnn_detection',
-            'dnn_example_image_width': str(960),
-            'dnn_example_image_height': str(544)
+            'dnn_example_msg_pub_topic_name': '/hobot_dnn_detection'
         }.items()
     )
 
